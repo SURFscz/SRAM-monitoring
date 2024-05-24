a91bb3260b051ebb0502da1a064688dbbfe2345f
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2024-05-24 09:40:03 +0200

    make job a bit more verbose

--- run_sram_monitoring.sh
+++ run_sram_monitoring.sh
@@ -10,12 +10,20 @@ if [ ! -f ${ENV}.yml ]; then
   exit 1
 fi
 
+echo "Starting chrome container"
 docker run --shm-size=2g --rm -d --name chrome -p 4444:4444 selenium/standalone-chrome >/dev/null 2>&1
+i=0
 while ! curl --output /dev/null --silent --head http://localhost:4444/wd/hub/status; do
-#   echo -n "."
-  sleep 1
+    echo -n "."
+    sleep 1
+    i=$(( $i + 1 ))
+    if [ $i -gt 60 ]
+    then
+        echo "TIMEOUT!"
+        exit 1
+    fi
 done
-# echo " Up!"
+echo " Up!"
 
 LOGFILE="status/${ENV}.log"
 
