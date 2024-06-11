d65e2db85153c5e1036577f09eb0e00663db444b
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2024-06-11 16:33:40 +0200

    Remove old container

--- run_sram_monitoring.sh
+++ run_sram_monitoring.sh
@@ -18,16 +18,14 @@ if [ ! -f ${ENV}.yml ]; then
 	exit 1
 fi
 
+echo "Stopping container"
+docker container rm -f browser || true
 echo "Starting $BROWSER container"
-if [ "$BROWSER" = "chrome" ]; then
-	docker run --shm-size=2g --rm -d --name browser -p 4444:4444 -p 7900:7900 selenium/standalone-chrome >/dev/null 2>&1
-fi
-if [ "$BROWSER" = "firefox" ]; then
-	docker run --shm-size=2g --rm -d --name browser -p 4444:4444 -p 7900:7900 selenium/standalone-firefox >/dev/null 2>&1
-fi
+docker run --shm-size=2g --rm -d --name browser -p 4444:4444 -p 7900:7900 selenium/standalone-${BROWSER}
 
 i=0
-while ! curl --output /dev/null --silent --head http://localhost:4444/wd/hub/status; do
+while ! curl --output /dev/null --silent --head http://localhost:4444/wd/hub/status
+do
 	echo -n "."
 	sleep 1
 	i=$(( $i + 1 ))
@@ -62,4 +60,5 @@ cat ${LOGFILE}.new
 mv ${LOGFILE}.new ${LOGFILE}
 
 docker stop browser >/dev/null 2>&1
+docker container rm browser >/dev/null 2>&1
 echo "End of  $BROWSER test"
